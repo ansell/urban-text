d5bb0b09124239aed4db628e49ac4e9fa019c74d
Peter Ansell <p_ansell@yahoo.com>
2016-09-28 11:03:23 +1000

    Initial stub for UrbanTextReporter
    Signed-off-by: Peter Ansell <p_ansell@yahoo.com>

---
+++ src/main/java/com/github/ansell/text/UrbanTextReporter.java
@@ -0,0 +1,52 @@
+/**
+ * 
+ */
+package com.github.ansell.text;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import joptsimple.OptionException;
+import joptsimple.OptionParser;
+import joptsimple.OptionSet;
+import joptsimple.OptionSpec;
+
+/**
+ * Investigates which text encodings a file may be living in and reports back.
+ * 
+ * @author Peter Ansell p_ansell@yahoo.com
+ */
+public class UrbanTextReporter {
+
+	public static void main(String... args) throws Exception {
+		final OptionParser parser = new OptionParser();
+
+		final OptionSpec<Void> help = parser.accepts("help").forHelp();
+		final OptionSpec<File> input = parser.accepts("input").withRequiredArg().ofType(File.class).required()
+				.describedAs("The input file to be investigates.");
+
+		OptionSet options = null;
+
+		try {
+			options = parser.parse(args);
+		} catch (final OptionException e) {
+			System.out.println(e.getMessage());
+			parser.printHelpOn(System.out);
+			throw e;
+		}
+
+		if (options.has(help)) {
+			parser.printHelpOn(System.out);
+			return;
+		}
+
+		final Path inputPath = input.value(options).toPath();
+		if (!Files.exists(inputPath)) {
+			throw new FileNotFoundException("Could not find input CSV file: " + inputPath.toString());
+		}
+
+	}
+
+}
